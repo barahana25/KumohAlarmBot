976ef82201b39a0add7745416516844fd139a561
이주형 <barahana10@kumoh.ac.kr>
2025-09-08 20:30:17 +0900

    이름 수정

--- bot/cogs/about.py
+++ bot/cogs/about.py
@@ -12,7 +12,7 @@ class About(commands.Cog):
     async def about(self,interaction: discord.Interaction):
         """ 봇에 대한 소개 """
         embed=discord.Embed(title="봇 정보", description="금오공대의 여러가지 정보를 알려주는 봇입니다.", color=color_code)
-        embed.add_field(name="개발자", value="25 이주형", inline=True)
+        embed.add_field(name="개발자", value="25학번", inline=True)
         embed.add_field(name="관련 링크", value="[Github](https://github.com/barahana25/Kumoh-alarm)", inline=True)
         # embed.set_footer(text=BOT_NAME_TAG_VER)
         await interaction.response.send_message(embed=embed)
